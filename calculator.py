5a54de66da5c4d555173394549b9d2053eddc6e7
Hesham Al Nukhailan <halnukhailan@gmail.com>
2019-09-11 22:01:03 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+first = input("Enter the first number:")
+second = input("Enter the second number:")
+
+def userinput
+if userinput(first,second):
+---    print(calculate_age(year,month,day), " Years") ----
+else:
+    print("input is invalid, kindly try again.")
