6b4f8d4d9d3aa3e6b91f2a2107549e9675e25d99
Hesham Al Nukhailan <halnukhailan@gmail.com>
2019-09-24 20:41:02 +0300

    recruitment progran

---
+++ recruitment_program.py
@@ -0,0 +1,42 @@
+print("Welcome to the special recruitment program, please answer the following questions:")
+print("")
+
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+experience = int(input("How many years of experience do you have? "))
+
+skills = ["Python", "C++", "Javascript", "Juggling","Running","Eating"] #1
+
+cv = {} #2
+cv = {
+	"name":name,#3
+	"age":age,#4
+	"experience":experience,#5
+	"skills":[]#6
+} 
+# cv["skills"] =[] #6
+
+
+for x in skills:#7
+	i = 1 + skills.index(x)
+	print(i,x)
+
+
+add = input("Choose a skill from above by entering its number: ")#8
+cv["skills"].append(add)#8
+
+add = input("Choose another skill from above by entering its number: ")#9
+cv["skills"].append(add)#9
+
+
+if cv["age"] > 25 and cv["experience"] > 2 and "Python" in cv["skills"]:
+		print("You have been accepted! %s" % name.title())
+else:
+	print("you're not accepted, get out!")
+
+
+
+
+#print(cv)
+
+
